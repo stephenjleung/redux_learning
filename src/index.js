a73dab423e87b31dcc209d48af736a472e6348ba
steponahen <steponahen@yahoo.com>
2016-04-29 23:47:59 +0000

    rendered my first component. added youtube api key to index.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    import React from 'react';
+    import ReactDOM from 'react-dom';
+    
+    const API_KEY = 'AIzaSyBqqgGFdjSw6uSMNa_JqrjT8U85-BCZnwI';
+    
+    // Create a new component.  This component should produce some HTML.
+    
+    const App = () => {
+        return <div>Hi!</div>;
+            
+    }
+    
+    
+    // Take this component's generated HTML and put it on the page (in the DOM)
+    
+    ReactDOM.render(<App />, document.querySelector('.container'));
