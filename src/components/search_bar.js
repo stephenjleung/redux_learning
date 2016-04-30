729b3833142420c17b7aefc24aef317dc4a4d432
steponahen <steponahen@yahoo.com>
2016-04-30 03:01:13 +0000

    introduced State in the searchbar.  onChange, Display the current state in html plaintext.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,16 +1,21 @@
     import React, { Component } from 'react';
 
     class SearchBar extends Component {
-      render() {
-        return <input onChange={event => console.log(event.target.value)} />;
-      }
-      
-      /*onInputChange(event) {
-          
-        console.log(event.target.value);
-          
-      }*/
+        constructor(props) {
+            super(props);
+            
+            this.state = { term: ''};
+        }
         
+        render() {
+            return (
+                <div>
+                    <input onChange={event => this.setState({ term: event.target.value })} />
+                    Value of the input: {this.state.term}
+                </div>
+                
+            );
+        }
     }
     
     export default SearchBar;
