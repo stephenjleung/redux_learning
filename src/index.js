e6c41f4df7c1dbaaa892f755f34b59bda1f8d046
steponahen <steponahen@yahoo.com>
2016-04-30 01:01:42 +0000

    added a basic search input using search_bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
     import React from 'react';
     import ReactDOM from 'react-dom';
     
+    import SearchBar from './components/search_bar';
+    
     const API_KEY = 'AIzaSyBqqgGFdjSw6uSMNa_JqrjT8U85-BCZnwI';
     
     // Create a new component.  This component should produce some HTML.
     
     const App = () => {
-        return <div>Hi!</div>;
+        return (
+          <div>
+            <SearchBar />
+          </div>
+        );
             
     }
     
