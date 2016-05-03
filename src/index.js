59f3cbe5ee6668f24e6b699ac0e9d7d0080c5d9e
steponahen <steponahen@yahoo.com>
2016-05-03 22:22:43 +0000

    added debounced version of videoSearch to throttle down instant search. installed lodash.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
     
+    import _ from 'lodash';
     import React, { Component } from 'react';
     import ReactDOM from 'react-dom';
     import YTSearch from 'youtube-api-search';
@@ -38,9 +39,11 @@
       
       
       render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) },300);
+        
         return (
           <div>
-            <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+            <SearchBar onSearchTermChange={videoSearch}/>
             <VideoDetail video={this.state.selectedVideo}/>
             <VideoList 
               onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
