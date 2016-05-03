b8ea3eefe16d33a935b69d067b6a699415c62281
steponahen <steponahen@yahoo.com>
2016-05-03 18:21:39 +0000

    added styling to style.css.  added videoSearch function to update video list after every keypress in searchbar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,18 +3,23 @@
     class SearchBar extends Component {
         constructor(props) {
             super(props);
-            
             this.state = { term: ''};
         }
         
         render() {
             return (
-                <div>
+                <div className="search-bar">
                     <input 
                         value={this.state.term}
-                        onChange={event => this.setState({ term: event.target.value })} />
+                        onChange={event => this.onInputChange(event.target.value)} />
                 </div>
             );
+        }
+        
+        
+        onInputChange(term) {
+            this.setState({ term });
+            this.props.onSearchTermChange(term);
         }
     }
     
